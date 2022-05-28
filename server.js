66eaab34df8b7ac258a5dd2608e98e66f865a00c
André Rds <andrerds.souza@gmail.com>
2022-05-28 10:50:47 -0300

    add nova url

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var port = process.env.PORT || 8080;
 // use originWhitelist instead.
 var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
 // var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);     
-var originWhitelist = ['http://localhost:8100', 'http://localhost', 'capacitor://localhost']
+var originWhitelist = ['http://localhost:8100', 'http://localhost:4200', 'http://localhost', 'capacitor://localhost']
 
 function parseEnvList(env) {
   if (!env) {
